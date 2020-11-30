1e11449833c8a46f9d9d377cf11d71f303f36dba
David Glass <glassd@Davids-MacBook-Pro.local>
2020-11-30 15:51:59 -0500

    Adding my frist compnents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent.js';
+import CounterButton from './CounterButton.js';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <MyComponent name='Dave'/>
+        <CounterButton/>
         <a
           className="App-link"
           href="https://reactjs.org"
